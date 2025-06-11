4b7b5a3a7301075a8b6ac7256f4e87e8e0df6420
Nium777 <nium7777@gmail.com>
2025-06-11 16:21:08 +0900

    amtime_rensyuu
    오후수업은~~~ 망했네~~~

---
+++ Unitybasic2/Assets/script/RanPy.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RanPy : MonoBehaviour
+{
+    [SerializeField] Image img_BG;
+    [SerializeField] Image[] img_chara;
+
+    [SerializeField] TextMeshProUGUI text_name;
+    [SerializeField] TextMeshProUGUI text_nametitle;
+    [SerializeField] TextMeshProUGUI text_box;
+    [SerializeField] Button Button_next;
+
+
+    void Start()
+    {
+        
+    }
+}
